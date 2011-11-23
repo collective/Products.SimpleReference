14b33501fad7e3e22a3b4405a0935ec885b0104d
petschki <peter.mathis@kombinat.at>
2011-11-23 09:57:31 +0000

    Prepare Products.SimpleReference 0.1a2.

--- Products/SimpleReference/browser/reference.py
+++ Products/SimpleReference/browser/reference.py
@@ -1,26 +1,27 @@
 from Acquisition import aq_inner
-from zope.component import getMultiAdapter
-
+from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.SimpleReference.browser.interfaces import IImageControls, \
+    IFileControls, IViewletFileListing, IViewletImageListing
+from zope.component import getMultiAdapter
 
-from interfaces import *
 
 class Reference(BrowserView):
     """ functions for reference browser popup
     """
-    
+
     def search_types(self):
         req_type = self.request.get('type')
         return (req_type, 'Folder')
-        
+
     def linkable_types(self):
         req_type = self.request.get('type')
         return (req_type, )
-        
+
     def browseable_types(self):
         return ('Folder', )
-    
+
     def add_reference(self, uid):
         """
         """
@@ -28,9 +29,9 @@ class Reference(BrowserView):
         portal_state = getMultiAdapter((context,self.request),
                                        name="plone_portal_state")
         portal = portal_state.portal()
-        attool = portal.archetype_tool
-        
-        at_obj = attool.lookupObject(uid)
+        refcat = getToolByName(portal, 'reference_catalog')
+
+        at_obj = refcat.lookupObject(uid)
         if at_obj:
             ref_type = '%sReference' % at_obj.portal_type
             at_obj_id = context.generateUniqueId(ref_type)
@@ -38,9 +39,9 @@ class Reference(BrowserView):
                         id=at_obj_id,
                         type_name=ref_type
                      )
-            
+
             new_obj = getattr(context, new_id)
-            
+
             if ref_type=='ImageReference':
                 new_obj.setImage(uid)
                 IVManager = IImageControls
@@ -53,17 +54,17 @@ class Reference(BrowserView):
                 IViewlet = IViewletFileListing
                 vmanager_name = 'simpleattachment.filecontrols'
                 viewlet_name = 'simpleattachment.filelisting'
-                
+
             new_obj.reindexObject()
-        
+
             # update controls viewletmanager and return html code
-            viewlet_manager = getMultiAdapter((self.context, 
-                                               self.request, 
+            viewlet_manager = getMultiAdapter((self.context,
+                                               self.request,
                                                self, ),
-                                              IVManager, 
+                                              IVManager,
                                               name=vmanager_name )
-                                              
+
             viewlet_manager.update()
             return viewlet_manager.render()
-            
+
         return 'error!'
