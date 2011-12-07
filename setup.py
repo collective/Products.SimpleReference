a5b31bc43e5e712dc4ba9dae89c35d4363c49d4a
petschki <peter.mathis@kombinat.at>
2011-12-07 13:31:22 +0000

    Prepare Products.SimpleReference 0.1b1.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.1a5'
+version = '0.1b1'
 
 long_description = (
     'Detailed Documentation\n'
